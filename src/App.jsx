6209006406682979bc9b7613d8ffb609321188ba
wkingsurin <wkingsurin@gmail.com>
2024-01-27 13:34:22 +0500

    added markup and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
 import "./App.css";
+import "normalize.css";
 
 export default function App() {
-  return <></>;
+  return (
+    <div className="wrapper">
+      <main className="main">
+        <div className="container">
+          <div className="content">
+            <div className="quote-box">
+              <p className="quote">
+                Genius is one percent inspiration and ninety-nine percent
+                perspiration.
+              </p>
+              <span className="author">Thomas Edison</span>
+            </div>
+            <button className="generate-button">Generate qoute</button>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
